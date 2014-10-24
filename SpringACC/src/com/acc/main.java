eec819cebb0f259ed6cc650f43be0c72cd5a0e19
lud200 <dludaya09@outlook.com>
2014-10-24 19:22:43 -0400

    spring

--- SpringACC/src/com/acc/main.java
+++ SpringACC/src/com/acc/main.java
@@ -1,7 +1,12 @@
 package com.acc;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-
+/*This container adds more enterprise-specific functionality such 
+ * as the ability to resolve textual messages from a properties file 
+ * and the ability to publish application events to interested event listeners. 
+ * This container is defined by the org.springframework.context.ApplicationContext interface.
+ * The ApplicationContext container includes all functionality of the BeanFactory container
+ */
 public class main {
 	public static void main(String args[]){
 		ApplicationContext context = new FileSystemXmlApplicationContext
